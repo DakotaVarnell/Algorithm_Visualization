41ecfdda2dd211123d05951780e099cffb57cd0a
nickadney <nadney@cub.uca.edu>
2022-09-29 13:58:06 -0500

    added bubble sort

--- data_visualization.py
+++ data_visualization.py
@@ -1,4 +1,18 @@
 import random
+#import matplotlib as mpl
+#import numpy as np
+
+#------------IMPORTANT------------#
+#matplotlib and numpy must be installed to successfully compile this program
+#if you are using anaconda:
+#conda install matplotlib
+#conda install numpy
+
+#if you are using pip:
+#pip install matplotlib
+#pip install numpy
+
+#it is best practice to use an environment rather than intall in the base env
 
 def generate_lists(n):
     
@@ -16,8 +30,45 @@ def generate_lists(n):
 
     return sorted_list, reversed_list, random_list
 
+def bubble_sort(myList):
+    for i in range(len(myList)-1):
+        for j in range(len(myList)-i-1):
+            if myList[j] > myList[j + 1]: #if the current value is greater than the next
+                myList[j], myList[j+1] = \
+                    myList[j + 1], myList[j] #swap them
+    return myList
 
-sorted, revers, ran = generate_lists(10)
-print(sorted)
-print(revers)
-print(ran)
+def bubble_sort_visualized(myList):
+    for i in range(len(myList)-1):
+        for j in range(len(myList)-i-1):
+            if myList[j] > myList[j + 1]: #if the current value is greater than the next
+                myList[j], myList[j+1] = \
+                    myList[j + 1], myList[j] #swap them
+    return myList
+
+def merge_sort():
+    return
+
+def merge_sort_visualized():
+    #implement visualization of merge sort
+    return
+
+def quick_sort():
+    return
+    
+def quick_sort_visualized():
+    #implement visualization of quick sort
+    return
+
+
+
+
+
+def __main__ ():
+    sorted, revers, ran = generate_lists(10)
+
+    # x = bubble_sort_visualized(ran)
+    # print(x)
+
+
+
