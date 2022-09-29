21f7393e7c71ff299832553745ce4ddcd9175669
DakotaVarnell <jvarnell1@cub.uca.edu>
2022-09-28 19:04:24 -0500

    added our first file

---
+++ data_visualization.py
@@ -0,0 +1,23 @@
+import random
+
+def generate_lists(n):
+    
+    sorted_list = []
+    random_list = []
+    for i in range(n):
+        sorted_list.append(i + 1)
+        random_list.append(i + 1)
+    
+    reversed_list = []
+    for i in reversed(sorted_list):
+        reversed_list.append(i) 
+
+    random.shuffle(random_list)
+
+    return sorted_list, reversed_list, random_list
+
+
+sorted, revers, ran = generate_lists(10)
+print(sorted)
+print(revers)
+print(ran)
